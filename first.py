9a373b55e60923e44d852d1735cbfa90397bacb6
Ragavjordan23 <87418770+Ragavjordan23@users.noreply.github.com>
2021-12-30 22:24:24 +0530

    Coded
    Entry Verified

--- first.py
+++ first.py
@@ -1,2 +1,24 @@
-name = 'ragav'
-print(name.upper())
+#Operator Chaining
+age1 = 10
+age2 = 12
+age3 = 14
+print(age1<age2<age3) # output: True
+print(10==20==30)  # output: False
+
+#identity operator
+no1 = 10
+no2 = 10
+print(no1 is no2) # output: True
+print(no1 == no2) # output: True
+
+#Escape Characters
+
+print("Hi\t GoodMorning") 
+print("Hi\n GoodMorning")
+
+#output: 
+# Hi   GoodMorning
+# Hi
+#      GoodMorning
+
+print('I\'m good') # output: I'm good
